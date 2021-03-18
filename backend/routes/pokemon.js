af71d4ecca465e65c41241b35b81ae2120e89731
Assaf Kaner <kaner.assaf@gmail.com>
2021-03-18 15:45:10 +0200

    use map instead of for each

--- backend/routes/pokemon.js
+++ backend/routes/pokemon.js
@@ -6,10 +6,7 @@ const pokemon = Router();
 pokemon.get("/:name", async (req, res) => {
   const pokeName = req.params.name
   const { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokeName}`)
-    const pokeTypes = [];
-    data.types.forEach((pokeType) => {
-      pokeTypes.push(pokeType.type.name);
-    })
+    const pokeTypes = data.types.map((pokeType) => pokeType.type.name);
     const details = {
       name: data.name,
       id: data.id,
