80bc03c4d39819ef54aea70545a6dcefeadaa83e
Assaf Kaner <kaner.assaf@gmail.com>
2021-03-18 16:53:27 +0200

    add get list by pokemon type

--- backend/routes/type.js
+++ backend/routes/type.js
@@ -1,9 +1,18 @@
 const { Router } = require("express");
+const axios = require('axios').default;
 
 const type = Router();
 
-type.get("/", (req, res) => {
-  res.send("type route");
+type.get("/:name", async (req, res) => {
+  const typeName = req.params.name;
+  const { data } = await axios.get(`https://pokeapi.co/api/v2/type/${typeName}/`);
+  const pokeArray = data.pokemon.map(obj => obj.pokemon);
+  const details = {
+    type: data.name,
+    pokemons: pokeArray,
+    id: data.id,
+  }
+  res.json(details);
 });
 
 module.exports = type;
