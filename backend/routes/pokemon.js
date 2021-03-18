abb530a369e4ee14d51ead2486b31fad12932817
Assaf Kaner <kaner.assaf@gmail.com>
2021-03-18 15:35:48 +0200

    get pokemon details from pokeAPI

--- backend/routes/pokemon.js
+++ backend/routes/pokemon.js
@@ -1,9 +1,23 @@
 const { Router } = require("express");
+const axios = require('axios').default;
 
 const pokemon = Router();
 
 pokemon.get("/:name", async (req, res) => {
-  res.send("Pokemon route");
+  const pokeName = req.params.name
+  const { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokeName}`)
+    const pokeTypes = [];
+    data.types.forEach((pokeType) => {
+      pokeTypes.push(pokeType.type.name);
+    })
+    const details = {
+      name: data.name,
+      id: data.id,
+      height: data.height,
+      weight: data.weight,
+      types: pokeTypes,
+    }
+    res.send(details);
 });
 
 module.exports = pokemon;
