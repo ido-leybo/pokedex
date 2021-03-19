a24b7b5c697605b430c1c16fa8331cde9dfa5a99
Ido Leybovitch <idoleybo01@gmail.com>
2021-03-19 10:34:55 +0200

    add image of the pokemon and event
    * when search pokemon its update the current image
    * onMouseOver
    * onMouseLeave

--- client/pokedex-app/src/App.js
+++ client/pokedex-app/src/App.js
@@ -17,21 +17,45 @@ const initialDetails = [
 function App() {
   const [detailsState, setDetails] = useState(initialDetails);
   const [listState, setList] = useState([]);
+  const [image, setImage] = useState(null);
 
   const onClick = async (event) => {
     const { value } = event.target.parentElement.children[0];
     console.log(value);
+    let {
+      src,
+    } = event.target.parentElement.parentElement.children[2].children[1];
     const pokeData = await axios.get(
       `http://localhost:3001/api/pokemon/${value}`
     );
+    src = pokeData.data.sprites.front;
+    setImage(src);
     setDetails(pokeData.data);
-    console.log(detailsState);
   };
+
+  const onOver = (event) => {
+    let src = event.target.src;
+    src = detailsState.sprites.back;
+    event.target.src = src;
+    setDetails(detailsState);
+  };
+  const onLeave = (event) => {
+    let src = event.target.src;
+    src = detailsState.sprites.front;
+    event.target.src = src;
+    setDetails(detailsState);
+  };
+
   return (
     <div className="App">
       <h1>Pokedex</h1>
       <SearchArea onClick={onClick} />
-      <Details details={detailsState} />
+      <Details
+        details={detailsState}
+        renderImage={image}
+        onOver={onOver}
+        onLeave={onLeave}
+      />
       <List list={listState} />
     </div>
   );
--- client/pokedex-app/src/components/Details.js
+++ client/pokedex-app/src/components/Details.js
@@ -1,14 +1,15 @@
 import React from "react";
 
-export default function Details(props) {
+export default function Details({ details, renderImage, onOver, onLeave }) {
   return (
     <div className="details">
       <ul>
-        <li>Name: {props.details.name}</li>
-        <li>Height: {props.details.height}</li>
-        <li>Weight: {props.details.height}</li>
-        <li>Types: {props.details.types}</li>
+        <li>Name: {details.name}</li>
+        <li>Height: {details.height}</li>
+        <li>Weight: {details.weight}</li>
+        <li>Types: {details.types}</li>
       </ul>
+      <img src={renderImage} onMouseOver={onOver} onMouseLeave={onLeave} />
     </div>
   );
 }
