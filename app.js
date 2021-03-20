5d5cb78163b4fbb89521d7230973cb9dd28c9dd5
Assaf Kaner <kaner.assaf@gmail.com>
2021-03-20 20:55:45 +0200

    prepare for heroku

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const cors = require("cors");
+const path = require('path');
 const api = require("./routes");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static("build"));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+})
 
 app.use("/api", api);
 
